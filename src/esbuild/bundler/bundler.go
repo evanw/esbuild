4064f1cc842b2d3e004fabed77724e804a1550f3
Evan Wallace <evan.exe@gmail.com>
2020-04-21 00:08:56 -0700

    move the runtime code to a separate file

--- src/esbuild/bundler/bundler.go
+++ src/esbuild/bundler/bundler.go
@@ -10,6 +10,7 @@ import (
 	"esbuild/parser"
 	"esbuild/printer"
 	"esbuild/resolver"
+	"esbuild/runtime"
 	"fmt"
 	"sort"
 	"strings"
@@ -119,7 +120,7 @@ func ScanBundle(
 			Index:        runtimeSourceIndex,
 			AbsolutePath: "<runtime>",
 			PrettyPath:   "<runtime>",
-			Contents:     runtime,
+			Contents:     runtime.Code,
 		}
 		sources = append(sources, source)
 		files = append(files, file{})
@@ -1617,40 +1618,3 @@ func removeTrailing(x []byte, c byte) []byte {
 }
 
 const runtimeSourceIndex = 0
-const runtime = `
-	let __defineProperty = Object.defineProperty
-
-	// This holds the exports for all modules that have been evaluated
-	let __modules = {}
-
-	let __require = (id, module) => {
-		module = __modules[id]
-		if (!module) {
-			module = __modules[id] = {exports: {}}
-			__commonjs[id](module.exports, module)
-		}
-		return module.exports
-	}
-
-	let __import = (module, exports) => {
-		module = __require(module)
-		if (module && module.__esModule) {
-			return module
-		}
-		exports = Object(module)
-		if (!('default' in exports)) {
-			__defineProperty(exports, 'default', { value: module, enumerable: true })
-		}
-		return exports
-	}
-
-	let __export = (target, all) => {
-		__defineProperty(target, '__esModule', { value: true })
-		for (let name in all) {
-			__defineProperty(target, name, { get: all[name], enumerable: true })
-		}
-	}
-
-	// This will be filled in with the CommonJS module map
-	let __commonjs
-`
---
+++ src/esbuild/runtime/runtime.go
@@ -0,0 +1,39 @@
+package runtime
+
+const Code = `
+	let __defineProperty = Object.defineProperty
+
+	// This holds the exports for all modules that have been evaluated
+	let __modules = {}
+
+	let __require = (id, module) => {
+		module = __modules[id]
+		if (!module) {
+			module = __modules[id] = {exports: {}}
+			__commonjs[id](module.exports, module)
+		}
+		return module.exports
+	}
+
+	let __import = (module, exports) => {
+		module = __require(module)
+		if (module && module.__esModule) {
+			return module
+		}
+		exports = Object(module)
+		if (!('default' in exports)) {
+			__defineProperty(exports, 'default', { value: module, enumerable: true })
+		}
+		return exports
+	}
+
+	let __export = (target, all) => {
+		__defineProperty(target, '__esModule', { value: true })
+		for (let name in all) {
+			__defineProperty(target, name, { get: all[name], enumerable: true })
+		}
+	}
+
+	// This will be filled in with the CommonJS module map
+	let __commonjs
+`
