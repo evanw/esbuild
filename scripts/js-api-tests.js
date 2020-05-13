af8f58754d924cd041b2e1a54da713e2160f8531
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2020-05-13 17:59:59 +0700

    add encoding

--- scripts/js-api-tests.js
+++ scripts/js-api-tests.js
@@ -70,7 +70,7 @@ let tests = {
     const { js } = await service.transform(`<svg width="100" height="100">
 <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
 </svg>`, { loader: 'svg' })
-    assert.strictEqual(js, `module.exports = 'data:image/svg+xml;<svg width="100" height="100"><circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></svg>';\n`)
+    assert.strictEqual(js, `module.exports = 'data:image/svg+xml;utf-8,<svg width="100" height="100"><circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></svg>';\n`)
   },
 }
 
