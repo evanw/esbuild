a66456e83feb2eaf729797d838e2e606c39c1e51
valaymerick <valaymerick@gmail.com>
2020-07-05 12:20:54 +0200

    Reuse the unicode isSpace func

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -543,32 +543,10 @@ func IsIdentifierContinue(codePoint rune) bool {
 
 // See the "White Space Code Points" table in the ECMAScript standard
 func IsWhitespace(codePoint rune) bool {
-	switch codePoint {
+	switch {
 	case
-		'\u0009', // character tabulation
-		'\u000B', // line tabulation
-		'\u000C', // form feed
-		'\u0020', // space
-		'\u00A0', // no-break space
-
-		// Unicode "Space_Separator" code points
-		'\u1680', // ogham space mark
-		'\u2000', // en quad
-		'\u2001', // em quad
-		'\u2002', // en space
-		'\u2003', // em space
-		'\u2004', // three-per-em space
-		'\u2005', // four-per-em space
-		'\u2006', // six-per-em space
-		'\u2007', // figure space
-		'\u2008', // punctuation space
-		'\u2009', // thin space
-		'\u200A', // hair space
-		'\u202F', // narrow no-break space
-		'\u205F', // medium mathematical space
-		'\u3000', // ideographic space
-
-		'\uFEFF': // zero width non-breaking space
+		unicode.IsSpace(codePoint),
+		codePoint == '\uFEFF': // zero width non-breaking space
 		return true
 
 	default:
