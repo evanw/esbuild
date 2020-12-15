5c22b6c5c3b6ae404b3f09409cfc5fbfe47fdbbb
Evan Wallace <evan.exe@gmail.com>
2020-12-15 00:58:10 -0800

    convert browser "startService" to async

--- lib/browser.ts
+++ lib/browser.ts
@@ -26,83 +26,81 @@ export const transformSync: typeof types.transformSync = () => {
   throw new Error(`The "transformSync" API only works in node`);
 };
 
-export const startService: typeof types.startService = common.referenceCountedService(options => {
+export const startService: typeof types.startService = common.referenceCountedService(async (options) => {
   if (!options) throw new Error('Must provide an options object to "startService"');
   options = common.validateServiceOptions(options)!;
   let wasmURL = options.wasmURL;
   let useWorker = options.worker !== false;
   if (!wasmURL) throw new Error('Must provide the "wasmURL" option');
   wasmURL += '';
-  return fetch(wasmURL).then(res => {
-    if (!res.ok) throw new Error(`Failed to download ${JSON.stringify(wasmURL)}`);
-    return res.arrayBuffer();
-  }).then(wasm => {
-    let code = `{` +
-      `let global={};` +
-      `for(let o=self;o;o=Object.getPrototypeOf(o))` +
-      `for(let k of Object.getOwnPropertyNames(o))` +
-      `global[k]=self[k];` +
-      WEB_WORKER_SOURCE_CODE +
-      `}`
-    let worker: {
-      onmessage: ((event: any) => void) | null
-      postMessage: (data: Uint8Array | ArrayBuffer) => void
-      terminate: () => void
-    }
+  let res = await fetch(wasmURL);
+  if (!res.ok) throw new Error(`Failed to download ${JSON.stringify(wasmURL)}`);
+  let wasm = await res.arrayBuffer();
+  let code = `{` +
+    `let global={};` +
+    `for(let o=self;o;o=Object.getPrototypeOf(o))` +
+    `for(let k of Object.getOwnPropertyNames(o))` +
+    `global[k]=self[k];` +
+    WEB_WORKER_SOURCE_CODE +
+    `}`
+  let worker: {
+    onmessage: ((event: any) => void) | null
+    postMessage: (data: Uint8Array | ArrayBuffer) => void
+    terminate: () => void
+  }
 
-    if (useWorker) {
-      // Run esbuild off the main thread
-      let blob = new Blob([code], { type: 'application/javascript' })
-      worker = new Worker(URL.createObjectURL(blob))
-    } else {
-      // Run esbuild on the main thread
-      let fn = new Function('postMessage', code + `var onmessage; return m => onmessage(m)`)
-      let onmessage = fn((data: Uint8Array) => worker.onmessage!({ data }))
-      worker = {
-        onmessage: null,
-        postMessage: data => onmessage({ data }),
-        terminate() {
-        },
-      }
-    }
-
-    worker.postMessage(wasm)
-    worker.onmessage = ({ data }) => readFromStdout(data)
-
-    let { readFromStdout, afterClose, service } = common.createChannel({
-      writeToStdin(bytes) {
-        worker.postMessage(bytes)
-      },
-      isSync: false,
-      isBrowser: true,
-    })
-
-    return {
-      build: (options: types.BuildOptions): Promise<any> =>
-        new Promise<types.BuildResult>((resolve, reject) =>
-          service.buildOrServe('build', null, options, false, (err, res) =>
-            err ? reject(err) : resolve(res as types.BuildResult))),
-      transform: (input, options) => {
-        input += '';
-        return new Promise((resolve, reject) =>
-          service.transform('transform', input, options || {}, false, {
-            readFile(_, callback) { callback(new Error('Internal error'), null); },
-            writeFile(_, callback) { callback(null); },
-          }, (err, res) => err ? reject(err) : resolve(res!)))
-      },
-      serve() {
-        throw new Error(`The "serve" API only works in node`)
-      },
-      buildSync() {
-        throw new Error(`The "buildSync" API only works in node`);
-      },
-      transformSync() {
-        throw new Error(`The "transformSync" API only works in node`);
-      },
-      stop() {
-        worker.terminate()
-        afterClose()
+  if (useWorker) {
+    // Run esbuild off the main thread
+    let blob = new Blob([code], { type: 'application/javascript' })
+    worker = new Worker(URL.createObjectURL(blob))
+  } else {
+    // Run esbuild on the main thread
+    let fn = new Function('postMessage', code + `var onmessage; return m => onmessage(m)`)
+    let onmessage = fn((data: Uint8Array) => worker.onmessage!({ data }))
+    worker = {
+      onmessage: null,
+      postMessage: data => onmessage({ data }),
+      terminate() {
       },
     }
+  }
+
+  worker.postMessage(wasm)
+  worker.onmessage = ({ data }) => readFromStdout(data)
+
+  let { readFromStdout, afterClose, service } = common.createChannel({
+    writeToStdin(bytes) {
+      worker.postMessage(bytes)
+    },
+    isSync: false,
+    isBrowser: true,
   })
+
+  return {
+    build: (options: types.BuildOptions): Promise<any> =>
+      new Promise<types.BuildResult>((resolve, reject) =>
+        service.buildOrServe('build', null, options, false, (err, res) =>
+          err ? reject(err) : resolve(res as types.BuildResult))),
+    transform: (input, options) => {
+      input += '';
+      return new Promise((resolve, reject) =>
+        service.transform('transform', input, options || {}, false, {
+          readFile(_, callback) { callback(new Error('Internal error'), null); },
+          writeFile(_, callback) { callback(null); },
+        }, (err, res) => err ? reject(err) : resolve(res!)))
+    },
+    serve() {
+      throw new Error(`The "serve" API only works in node`)
+    },
+    buildSync() {
+      throw new Error(`The "buildSync" API only works in node`);
+    },
+    transformSync() {
+      throw new Error(`The "transformSync" API only works in node`);
+    },
+    stop() {
+      worker.terminate()
+      afterClose()
+    },
+  }
 });
