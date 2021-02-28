e133f4ecc6f7896403d1600f5b605985810eec65
susiwen8 <susiwen8@gmail.com>
2021-02-28 23:17:56 +0800

    chore: no hard code 'UseStrict'

--- internal/bundler/bundler.go
+++ internal/bundler/bundler.go
@@ -1839,7 +1839,7 @@ func (cache *runtimeCache) parseRuntime(options *config.Options) (source logger.
 		// Always do tree shaking for the runtime because we never want to
 		// include unnecessary runtime code
 		Mode:      config.ModeBundle,
-		UseStrict: true,
+		UseStrict: options.UseStrict,
 	}))
 	if log.HasErrors() {
 		msgs := "Internal error: failed to parse runtime:\n"
