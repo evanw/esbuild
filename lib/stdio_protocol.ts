ffe21706242ba6b2b10fee5c007674b42ec723d1
Justin Ridgewell <justin@ridgewell.name>
2021-03-25 03:58:43 -0400

    Ensure we only decode the TypedArray's view

--- lib/stdio_protocol.ts
+++ lib/stdio_protocol.ts
@@ -352,7 +352,10 @@ else if (typeof Buffer !== 'undefined') {
 
     return buffer;
   };
-  decodeUTF8 = bytes => Buffer.from(bytes.buffer).toString();
+  decodeUTF8 = bytes => {
+    let { buffer, byteOffset, byteLength } = bytes;
+    return Buffer.from(buffer, byteOffset, byteLength).toString();
+  }
 }
 
 else {
