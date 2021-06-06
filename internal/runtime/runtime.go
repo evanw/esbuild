236039d30bc0f9cfd8a4d3d36fcce26fc87adfdb
Evan Wallace <evan.exe@gmail.com>
2021-06-05 22:08:44 -0700

    runtime: check "for-of" not "=>" for es6 support

--- internal/runtime/runtime.go
+++ internal/runtime/runtime.go
@@ -16,7 +16,7 @@ import (
 const SourceIndex = uint32(0)
 
 func CanUseES6(unsupportedFeatures compat.JSFeature) bool {
-	return !unsupportedFeatures.Has(compat.Let) && !unsupportedFeatures.Has(compat.Arrow)
+	return !unsupportedFeatures.Has(compat.Let) && !unsupportedFeatures.Has(compat.ForOf)
 }
 
 func code(isES6 bool) string {
@@ -291,20 +291,20 @@ func code(isES6 bool) string {
 		// This is for the "binary" loader (custom code is ~2x faster than "atob")
 		export var __toBinaryNode = base64 => new Uint8Array(Buffer.from(base64, 'base64'))
 		export var __toBinary = /* @__PURE__ */ (() => {
-				var table = new Uint8Array(128)
-				for (var i = 0; i < 64; i++) table[i < 26 ? i + 65 : i < 52 ? i + 71 : i < 62 ? i - 4 : i * 4 - 205] = i
-				return base64 => {
-					var n = base64.length, bytes = new Uint8Array((n - (base64[n - 1] == '=') - (base64[n - 2] == '=')) * 3 / 4 | 0)
-					for (var i = 0, j = 0; i < n;) {
-						var c0 = table[base64.charCodeAt(i++)], c1 = table[base64.charCodeAt(i++)]
-						var c2 = table[base64.charCodeAt(i++)], c3 = table[base64.charCodeAt(i++)]
-						bytes[j++] = (c0 << 2) | (c1 >> 4)
-						bytes[j++] = (c1 << 4) | (c2 >> 2)
-						bytes[j++] = (c2 << 6) | c3
-					}
-					return bytes
+			var table = new Uint8Array(128)
+			for (var i = 0; i < 64; i++) table[i < 26 ? i + 65 : i < 52 ? i + 71 : i < 62 ? i - 4 : i * 4 - 205] = i
+			return base64 => {
+				var n = base64.length, bytes = new Uint8Array((n - (base64[n - 1] == '=') - (base64[n - 2] == '=')) * 3 / 4 | 0)
+				for (var i = 0, j = 0; i < n;) {
+					var c0 = table[base64.charCodeAt(i++)], c1 = table[base64.charCodeAt(i++)]
+					var c2 = table[base64.charCodeAt(i++)], c3 = table[base64.charCodeAt(i++)]
+					bytes[j++] = (c0 << 2) | (c1 >> 4)
+					bytes[j++] = (c1 << 4) | (c2 >> 2)
+					bytes[j++] = (c2 << 6) | c3
 				}
-			})()
+				return bytes
+			}
+		})()
 	`
 
 	return text
