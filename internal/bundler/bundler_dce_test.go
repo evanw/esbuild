5e05c568cae86f4273a464d1f9be6412f02848c7
Evan Wallace <evan.exe@gmail.com>
2021-12-31 04:37:56 +0000

    preserve syntax position behavior during inlining

--- internal/bundler/bundler_dce_test.go
+++ internal/bundler/bundler_dce_test.go
@@ -2388,3 +2388,74 @@ func TestInlineEmptyFunctionCalls(t *testing.T) {
 		},
 	})
 }
+
+func TestInlineFunctionCallBehaviorChanges(t *testing.T) {
+	dce_suite.expectBundled(t, bundled{
+		files: map[string]string{
+			"/entry.js": `
+				function empty() {}
+				function id(x) { return x }
+
+				export let shouldBeWrapped = [
+					id(foo.bar)(),
+					id(foo[bar])(),
+					id(foo?.bar)(),
+					id(foo?.[bar])(),
+
+					(empty(), foo.bar)(),
+					(empty(), foo[bar])(),
+					(empty(), foo?.bar)(),
+					(empty(), foo?.[bar])(),
+
+					id(eval)(),
+					id(eval)?.(),
+					(empty(), eval)(),
+					(empty(), eval)?.(),
+
+					id(foo.bar)` + "``" + `,
+					id(foo[bar])` + "``" + `,
+					id(foo?.bar)` + "``" + `,
+					id(foo?.[bar])` + "``" + `,
+
+					(empty(), foo.bar)` + "``" + `,
+					(empty(), foo[bar])` + "``" + `,
+					(empty(), foo?.bar)` + "``" + `,
+					(empty(), foo?.[bar])` + "``" + `,
+
+					delete id(foo),
+					delete id(foo.bar),
+					delete id(foo[bar]),
+					delete id(foo?.bar),
+					delete id(foo?.[bar]),
+
+					delete (empty(), foo),
+					delete (empty(), foo.bar),
+					delete (empty(), foo[bar]),
+					delete (empty(), foo?.bar),
+					delete (empty(), foo?.[bar]),
+
+					delete empty(),
+				]
+
+				export let shouldNotBeWrapped = [
+					id(foo)(),
+					(empty(), foo)(),
+
+					id(foo)` + "``" + `,
+					(empty(), foo)` + "``" + `,
+				]
+
+				export let shouldNotBeDoubleWrapped = [
+					delete (empty(), foo(), bar()),
+					delete id((foo(), bar())),
+				]
+			`,
+		},
+		entryPaths: []string{"/entry.js"},
+		options: config.Options{
+			Mode:         config.ModePassThrough,
+			AbsOutputDir: "/out",
+			MangleSyntax: true,
+		},
+	})
+}
