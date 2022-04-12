8a016d95e50392c54fc7611633495f33efe56b09
sapphi-red <green@sapphi.red>
2022-04-12 15:31:07 +0900

    fix "metadata returned by plugin is inconsistent" happening with pluginName

--- internal/resolver/resolver.go
+++ internal/resolver/resolver.go
@@ -187,11 +187,6 @@ func (old *ResolveResult) Compare(new *ResolveResult) (diff []string) {
 	var oldDiff []string
 	var newDiff []string
 
-	if old.PluginName != new.PluginName {
-		oldDiff = append(oldDiff, prettyPrintPluginName("-", "pluginName", old.PluginName))
-		newDiff = append(newDiff, prettyPrintPluginName("+", "pluginName", new.PluginName))
-	}
-
 	if !old.PathPair.Primary.IsEquivalentTo(new.PathPair.Primary) {
 		oldDiff = append(oldDiff, prettyPrintPath("-", "path", old.PathPair.Primary))
 		newDiff = append(newDiff, prettyPrintPath("+", "path", new.PathPair.Primary))
