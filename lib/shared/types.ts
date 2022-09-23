1bce251e80bdf93a912bc2bf98b7f1f4586724a7
翠 / green <green@sapphi.red>
2022-09-24 00:18:07 +0900

    fix transform tsconfigRaw type (#2568)

--- lib/shared/types.ts
+++ lib/shared/types.ts
@@ -252,11 +252,15 @@ export interface ServeResult {
 export interface TransformOptions extends CommonOptions {
   tsconfigRaw?: string | {
     compilerOptions?: {
+      alwaysStrict?: boolean,
+      importsNotUsedAsValues?: 'remove' | 'preserve' | 'error',
+      jsx?: 'react' | 'react-jsx' | 'react-jsxdev' | 'preserve',
       jsxFactory?: string,
       jsxFragmentFactory?: string,
-      useDefineForClassFields?: boolean,
-      importsNotUsedAsValues?: 'remove' | 'preserve' | 'error',
+      jsxImportSource?: string,
       preserveValueImports?: boolean,
+      target?: string,
+      useDefineForClassFields?: boolean,
     },
   };
 
