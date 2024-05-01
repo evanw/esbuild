2b426b7e315779d502c9376f86afc569c6fa78b5
Evan Wallace <evan.exe@gmail.com>
2024-04-30 20:21:28 -0400

    improve typescript experimental decorator errors

--- internal/js_parser/js_parser.go
+++ internal/js_parser/js_parser.go
@@ -2070,10 +2070,11 @@ func (p *parser) parseProperty(startLoc logger.Loc, kind js_ast.PropertyKind, op
 		p.lexer.Next()
 
 	case js_lexer.TPrivateIdentifier:
-		if !opts.isClass || (p.options.ts.Config.ExperimentalDecorators == config.True && len(opts.decorators) > 0) {
+		if p.options.ts.Parse && p.options.ts.Config.ExperimentalDecorators == config.True && len(opts.decorators) > 0 {
+			p.log.AddError(&p.tracker, p.lexer.Range(), "TypeScript experimental decorators cannot be used on private identifiers")
+		} else if !opts.isClass {
 			p.lexer.Expected(js_lexer.TIdentifier)
-		}
-		if opts.tsDeclareRange.Len != 0 {
+		} else if opts.tsDeclareRange.Len != 0 {
 			p.log.AddError(&p.tracker, opts.tsDeclareRange, "\"declare\" cannot be used with a private identifier")
 		}
 		name := p.lexer.Identifier
