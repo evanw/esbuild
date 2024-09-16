7c1e87cdfa0ebd6dd79d90c6bb8976b13735b0f5
Cameron Howey <chowey@ualberta.net>
2024-09-16 11:51:52 -0700

    Provide a flag to auto-prefix local css classes when minified

--- internal/config/config.go
+++ internal/config/config.go
@@ -474,6 +474,7 @@ type Options struct {
 	TreeShaking            bool
 	DropDebugger           bool
 	MangleQuoted           bool
+	LocalCSSPrefix         string
 	Platform               Platform
 	OutputFormat           Format
 	NeedsMetafile          bool
