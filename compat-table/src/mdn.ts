a35eceb40ff67d2b599555a15b75bc2298b590a6
Evan Wallace <evan.exe@gmail.com>
2025-12-16 14:24:53 -0500

    compat table: fix a type error with the new types

--- compat-table/src/mdn.ts
+++ compat-table/src/mdn.ts
@@ -1,6 +1,6 @@
 // This file processes data from https://developer.mozilla.org/en-US/docs/Web
 
-import bcd, { BrowserName, SupportBlock } from '@mdn/browser-compat-data'
+import bcd, { BrowserName, SimpleSupportStatement, SupportBlock } from '@mdn/browser-compat-data'
 import { CSSFeature, CSSPrefixMap, CSSProperty, Engine, JSFeature, PrefixData, Support, SupportMap } from './index'
 
 const supportedEnvironments: Record<string, Engine> = {
@@ -189,8 +189,8 @@ for (const fullKey in cssPrefixFeatures) {
     const engine = supportedEnvironments[env]
 
     if (engine) {
-      let entries = support[env as BrowserName]!
-      if (!Array.isArray(entries)) entries = [entries]
+      const initialEntries = support[env as BrowserName]!
+      let entries: SimpleSupportStatement[] = Array.isArray(initialEntries) ? initialEntries : [initialEntries]
 
       // Figure out which version this property can be used unprefixed, if any.
       // This assumes that support for these CSS properties is never removed.
